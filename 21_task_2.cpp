32686883f0a7fcc534a35cc0afa8c5e1ef5c12d3
MLR <lenar_sova87@mail.ru>
2024-01-25 21:40:36 +0300

    continued work on the function fillBuildingInfo

--- 21_task_2.cpp
+++ 21_task_2.cpp
@@ -8,61 +8,86 @@ using namespace std;
 struct Room
 {
 public:
-    enum RoomType { bedroom, kitchen, bathroom, restroom, playroom, living, main, undefined };
-    vector<string>roomNames = { "bedroom", "kitchen", "bathroom", "restroom", "playroom", "living", "main", "undefined" };
+    enum RoomType
+    {
+        bedroom,
+        kitchen,
+        bathroom,
+        restroom,
+        playroom,
+        living,
+        main,
+        undefined
+    };
+    vector<string> roomNames = {"bedroom", "kitchen", "bathroom", "restroom", "playroom", "living", "main", "undefined"};
     RoomType type = RoomType::undefined;
-    int length{ 2000 };
-    int width{ 2000 };
+    int length{2000};
+    int width{2000};
 };
 
 struct Floor
 {
 public:
-    enum FloorType { first, second, third, undefined };
-    vector<string> floorNames = { "first", "second", "third", "undefined" };
+    enum FloorType
+    {
+        first,
+        second,
+        third,
+        undefined
+    };
+    vector<string> floorNames = {"first", "second", "third", "undefined"};
     FloorType type = FloorType::undefined;
-    int height{ 2600 };
-    int maxRoomCount{ 4 };
-    vector<Room>rooms;
+    int height{2600};
+    int maxRoomCount{4};
+    vector<Room> rooms;
 };
-struct Building {
+struct Building
+{
 public:
-    enum class BuildingType { house, garage, shed, bathHouse, undefined };
-    vector<string> buildingNames = { "house", "garage", "shed", "bathHouse", "undefined" };
+    enum class BuildingType
+    {
+        house,
+        garage,
+        shed,
+        bathHouse,
+        undefined
+    };
+    vector<string> buildingNames = {"house", "garage", "shed", "bathHouse", "undefined"};
     BuildingType type = BuildingType::undefined;
-    bool isStove{ false };
-    int maxFloorInHouse{ 3 };
-    vector<Floor>floors;
+    bool isStove{false};
+    int maxFloorInHouse{3};
+    vector<Floor> floors;
+    int square { 0 };
 };
 struct Area
 {
 public:
-    int square{ 0 };
-    vector<Building>buildings;
+    int square{0};
+    vector<Building> buildings;
 };
 
 struct Vilage
 {
 public:
-    vector<Area>areas;
+    vector<Area> areas;
 };
 
-vector<string> splitString(string const& str, char const& separator)
+vector<string> splitString(string const &str, char const &separator)
 {
     vector<string> words;
     string word;
     stringstream ss(str);
     while (getline(ss, word, separator))
     {
-        if (word.length() != 0) {
+        if (word.length() != 0)
+        {
             words.push_back(word);
         }
     }
     return words;
 }
 
-
-string getUserString(const string& text)
+string getUserString(const string &text)
 {
     printf("%s", text.c_str());
     string user_input;
@@ -70,11 +95,12 @@ string getUserString(const string& text)
     return splitString(user_input, ' ')[0];
 }
 
-
-bool isNumeric(string& number)
+bool isNumeric(string &number)
 {
-    if (number.begin() == number.end()) return false;
-    auto it = find_if(number.begin(), number.end(), [](char const& c) {return !isdigit(c); });
+    if (number.begin() == number.end())
+        return false;
+    auto it = find_if(number.begin(), number.end(), [](char const &c)
+                      { return !isdigit(c); });
     return it == number.end();
 }
 
@@ -84,60 +110,80 @@ int getNumber(string text)
     while (true)
     {
         user_input = getUserString(text);
-        if (isNumeric(user_input)) break;
+        if (isNumeric(user_input))
+            break;
         cout << "Это должно быть число!\n";
     }
     return stoi(user_input);
 }
 
-struct Building {
-public:
-    enum class BuildingType { undefined, house, garage, shed, bathHouse};
-    vector<string> buildingNames = { "house", "garage", "shed", "bathHouse", "undefined" };
-    BuildingType type = BuildingType::undefined;
-    bool isStove{ false };
-    int maxFloorInHouse{ 3 };
-    vector<Floor>floors;
-};
+void buildingHaveStove(Building &build)
+{
+    cout << "Есть ли печь с трубой в здании?\n";
+    bool result{false};
+    string answer = getUserString("Введите (y) если есть : ");
+    result = (answer == "y" or answer == "у" or answer == "Y" or answer == "У");
+    build.isStove = result;
+}
 
-int getNumberRange(string text, int to, int from)
+int getNumberRange(string text, int from, int to)
 {
     int number;
-    do {
-        cout << "Введите число в диапозоне от " << to << " до " << from << endl;
+    do
+    {
+        cout << "Введите число в диапозоне от " << from << " до " << to << endl;
         number = getNumber(text);
-    } while (number < to or number > from);
+    } while (number < from or number > to);
     return number;
 }
 
-Building fillBildingInfo()
+void whatIsArea(Building &build)
+{
+    int square{ 0 };
+    do {
+    cout << "Введите размеры.";
+    int width = getNumber("Введите ширину : ");
+    int length = getNumber("Введите длину : ");
+    square = width * length;
+    } while(square <= 0);
+    cout << "Площадь равна : " << square;
+    build.square = square;
+}
+
+Building fillBuildingInfo()
 {
     Building newBuilding;
     cout << "Какого типа ваше строение:\n";
-    for(int i = 0; i < newBuilding.buildingNames.size(); ++i)
+    for (int i = 0; i < newBuilding.buildingNames.size(); ++i)
     {
         cout << i + 1 << ". " << newBuilding.buildingNames[i] << endl;
     }
     int typeBuilding = getNumberRange("Введите номер подходящего типа :", 1, newBuilding.buildingNames.size());
-    switch(typeBuilding){
-        case 1:
-        {
-            newBuilding.type = Building::BuildingType::house;
-        }
-        case 2:
-        {
-            newBuilding.type = Building::BuildingType::garage;
-        }
-        case 3:
-        {
-            newBuilding.type = Building::BuildingType::shed;
-        }
-        case 4:
-        {
-            newBuilding.type = Building::BuildingType::bathHouse;
-        }
-        default:
-            newBuilding.type = Building::BuildingType::undefined;
+    switch (typeBuilding)
+    {
+    case 1:
+    {
+        newBuilding.type = Building::BuildingType::house;
+        buildingHaveStove(newBuilding);
+    }
+    case 2:
+    {
+        newBuilding.type = Building::BuildingType::garage;
+        whatIsArea(newBuilding);
+    }
+    case 3:
+    {
+        newBuilding.type = Building::BuildingType::shed;
+        whatIsArea(newBuilding);
+    }
+    case 4:
+    {
+        newBuilding.type = Building::BuildingType::bathHouse;
+        buildingHaveStove(newBuilding);
+        whatIsArea(newBuilding);
+    }
+    default:
+        newBuilding.type = Building::BuildingType::undefined;
     }
     return newBuilding;
 }
@@ -145,23 +191,20 @@ Building fillBildingInfo()
 void fillingArea(Area &area)
 {
     string message;
-    if (area.buildings.empty()){
+    if (area.buildings.empty())
+    {
         message = "Введите количество зданий на участке : ";
     }
     else
     {
-        message = "На участке уже зарегистрированно " 
-        + to_string(area.buildings.size())
-        + "домов\n. Общей площадью = " 
-        + to_string(area.square) 
-        + "\nСколько домов вы хотите добавить : ";
+        message = "На участке уже зарегистрированно " + to_string(area.buildings.size()) + "домов\n. Общей площадью = " + to_string(area.square) + "\nСколько домов вы хотите добавить : ";
     }
-        int countBildingsInArea = getNumber(message);
+    int countBildingsInArea = getNumber(message);
 
-        for(int i = 0; i < countBildingsInArea; ++i)
-        {
-            area.buildings.push_back(fillBildingInfo());
-        }
+    for (int i = 0; i < countBildingsInArea; ++i)
+    {
+        area.buildings.push_back(fillBuildingInfo());
+    }
 }
 void createMyVillage()
 {
@@ -170,17 +213,16 @@ void createMyVillage()
     myVilage.areas.resize(countAreas);
     bool wantFillingInData{true};
     int countFillingAreas{0};
-    while(wantFillingInData and countFillingAreas < countAreas)
+    while (wantFillingInData and countFillingAreas < countAreas)
     {
         int countArea = getNumber("Введите номер участка для заполнения : ");
         fillingArea(myVilage.areas[countArea]);
     }
-    
 }
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
     createMyVillage();
-	return 0;
+    return 0;
 }
